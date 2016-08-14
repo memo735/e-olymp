d996fa4476cff21bd522014b6bd9c234f126908a
memo735 <memo735@users.noreply.github.com>
2016-08-14 10:02:13 +0400

    Update 0044 - The number of ones - Единицы - Birlər - Одиниці.cpp

--- 0000-0999/0044 - The number of ones - Единицы - Birlər - Одиниці.cpp
+++ 0000-0999/0044 - The number of ones - Единицы - Birlər - Одиниці.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int dn[5000];
-
-int rec(int index)
-{
-	if (dn[index] != -1) return dn[index];
-	int ans = 1 + rec(index - 1);
-	for (int i = 2; i < index / 2; i++) ans = min(ans, rec(i) + rec(index - i));
-	for (int i = 2; i <= sqrt(index); i++) if (index % i == 0) ans = min(ans, rec(i) + rec(index / i));
-	return dn[index] = ans;
-}
-
-int main()
-{
-	int n;  cin >> n;
-	for (int i = 0; i <= n; i++) dn[i] = -1;
-	dn[0] = dn[1] = 1;
-	cout << rec(n) << endl;
+int main() {
+	int n, res[5001];
+	scanf("%d", &n);
+	res[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		res[i] = i;
+		for (int j = 1; 2 * j < i; j++)
+			if (res[j] + res[i - j] < res[i]) res[i] = res[j] + res[i - j];
+		for (int j = 2; j * j <= i; j++)
+			if (i % j == 0 && res[j] + res[i / j] < res[i]) res[i] = res[j] + res[i / j];
+	}
+	printf("%d\n", res[n]);
+	return 0;
 }
