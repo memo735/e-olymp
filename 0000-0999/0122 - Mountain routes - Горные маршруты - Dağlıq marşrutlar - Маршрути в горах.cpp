7a24f09d34163e7dd64a44c210aa27be406ae5c1
memo735 <memo735@users.noreply.github.com>
2016-08-14 10:37:38 +0400

    Update 0122 - Mountain routes - Горные маршруты - Dağlıq marşrutlar - Маршрути в горах.cpp

--- 0000-0999/0122 - Mountain routes - Горные маршруты - Dağlıq marşrutlar - Маршрути в горах.cpp
+++ 0000-0999/0122 - Mountain routes - Горные маршруты - Dağlıq marşrutlar - Маршрути в горах.cpp
@@ -1,74 +1,47 @@
-#include <stdio.h>
-#include <vector>
-#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
 using namespace std;
 
-struct Graph
+#define MAX 51
+int res, a, b, d, n, k, i, m[MAX][MAX], used[MAX];
+
+//Запускаем поиск в глубину из вершины v.
+//При этом известно, что длина пути от a до v уже равна depth.
+
+void dfs(int v, int depth)
 {
-	vector< vector<int> > AdjList;
-	int Size;
-	Graph(int n)
+	if (depth > d) return;
+
+	//Если мы пришли в вершину b, то найден еще один маршрут.
+	//Увеличиваем количество путей res на единицу и возвращаемся назад.
+	if (v == b)
 	{
-		vector<int> List;
-		for (int k = 0; k < n; k++) AdjList.push_back(List);
-		Size = n;
+		res++;
+		return;
 	}
+	used[v] = 1;
 
-	void add_vertex()
-	{
-		// вершина == целое число == номер этой вершины в списке вершин
-		Size++;
-		vector<int> List;
-		AdjList.push_back(List);
-	}
+	//Ищем вершину i, в которую можно попасть из v.
+	for (int i = 1; i <= n; i++)
+		if (m[v][i] && !used[i]) dfs(i, depth + 1);
 
-	void add_edge(int u, int v)
-	{		// u и v уже должны быть в списке вершин
-		AdjList[u].push_back(v);
-	}
+	//Поскольку происходит перебор вершин с возвратом, то следует установить вершину v как непройденную.
+	used[v] = 0;
+}
 
-	int simple_paths(int start, int end, int max_depth)
-	{
-		bool* on_path = (bool*)calloc(Size, sizeof(bool));
-		int cur_depth = 0;
-		int count = 0;
-		simple_paths(start, end, on_path, cur_depth, max_depth, count);
-		return count;
-	}
-
-	void simple_paths(int start, int end, bool* on_path, int& cur_depth, int max_depth, int& count)
-	{
-		if (cur_depth <= max_depth)
-		{
-			on_path[start] = true;
-			if (start == end) count++;
-			else
-				for (int k = 0; k < AdjList[start].size(); k++)
-				{
-					int v = AdjList[start][k];
-					if (!on_path[v])
-					{
-						cur_depth++;
-						simple_paths(v, end, on_path, cur_depth, max_depth, count);
-						on_path[v] = false;
-						cur_depth--;
-					}
-				}
-		}
-	}
-
-};
-
-int main()
-{
-	int n, k, a, b, d, tmp1, tmp2;
+//Ориентированный граф читаем в матрицу смежности m.
+int main() {
 	scanf("%d %d %d %d %d", &n, &k, &a, &b, &d);
-	Graph G(n);
-	for (int i = 0; i < k; i++)
+	memset(m, 0, sizeof m);
+	memset(used, 0, sizeof used);
+	for (res = i = 0; i < k; i++)
 	{
-		scanf("%d %d", &tmp1, &tmp2);
-		G.add_edge(tmp1 - 1, tmp2 - 1);
+		int a1, a2;
+		scanf("%d %d", &a1, &a2);
+		m[a1][a2] = 1;
 	}
-	printf("%d\n", G.simple_paths(a - 1, b - 1, d));
+	dfs(a, 0);
+	printf("%d\n", res);
 	return 0;
 }
