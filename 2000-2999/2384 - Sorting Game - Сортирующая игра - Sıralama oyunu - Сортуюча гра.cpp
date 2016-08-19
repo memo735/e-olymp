47b7816854c99220b311550db17730ad129c1876
memo735 <memo735@users.noreply.github.com>
2016-08-19 20:23:47 +0400

    Update and rename Problem2384_C++ to 2384 - Sorting Game - Сортирующая игра - Sıralama oyunu - Сортуюча гра.cpp

--- 2000-2999/2384 - Sorting Game - Сортирующая игра - Sıralama oyunu - Сортуюча гра.cpp
+++ 2000-2999/2384 - Sorting Game - Сортирующая игра - Sıralama oyunu - Сортуюча гра.cpp
@@ -1,73 +1,69 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int toint(string s){
+int toint(string s) {
 	int res = 0;
 	for (int i = 0; i<s.size(); i++) res = res * 10 + s[i] - 48;
 	return res;
 }
 
-string tostr(int a){
+string tostr(int a) {
 	string res = "";
-	while(a){
-		res += a%10+48;
-		a/=10;
+	while (a) {
+		res += a % 10 + 48;
+		a /= 10;
 	}
- 	reverse(res.begin(), res.end());
+	reverse(res.begin(), res.end());
 	return res;
 }
 
-
-
-int n,k,tmp,st,fin,i;
+int n, k, tmp, st, fin, i;
 map <int, int> dist;
-vector <int> que1,que2;
+vector <int> que1, que2;
 string ss, sstmp;
 
-void check(int x, int val){
-	if (!dist[x]){
+void check(int x, int val) {
+	if (!dist[x]) {
 		dist[x] = val;
-		que2.push_back(x);	
+		que2.push_back(x);
 	}
 }
 
-int main(){
-	while(cin){
+int main() {
+	while (cin) {
 		cin >> n >> k;
-		if (cin){
+		if (cin) {
 			st = fin = 0;
-			for (i = 0; i<n; i++) {
+			for (i = 0; i < n; i++) {
 				cin >> tmp;
 				st = st * 10 + tmp;
 				fin = fin * 10 + i + 1;
 			}
-
 			dist.clear();
 			que1.clear();
 			que2.clear();
-			check(st,1);
+			check(st, 1);
 			swap(que1, que2);
 			bool ans_found = false;
-
-			while(!que1.empty() && !ans_found){
-				for (i = 0; i<que1.size(); i++){
-					if (que1[i] == fin){
-						cout << dist[fin]-1 << endl;
+			while (!que1.empty() && !ans_found) {
+				for (i = 0; i < que1.size(); i++) {
+					if (que1[i] == fin) {
+						cout << dist[fin] - 1 << endl;
 						ans_found = true;
 						break;
 					}
 					ss = tostr(que1[i]);
-					for (int j = 0; j<n-k+1; j++){
+					for (int j = 0; j < n - k + 1; j++) {
 						sstmp = ss;
-						reverse(sstmp.begin()+j, sstmp.begin()+j+k);
-						check(toint(sstmp), dist[que1[i]]+1);			
+						reverse(sstmp.begin() + j, sstmp.begin() + j + k);
+						check(toint(sstmp), dist[que1[i]] + 1);
 					}
-				}	
+				}
 				que1.clear();
 				swap(que1, que2);
 			}
