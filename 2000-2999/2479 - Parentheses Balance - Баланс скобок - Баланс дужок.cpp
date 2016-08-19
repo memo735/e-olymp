806fada33a38a82e5c8f7dd97b65566ec38c11a8
memo735 <memo735@users.noreply.github.com>
2016-08-19 21:45:29 +0400

    Update and rename Problem2479_C++ to 2479 - Parentheses Balance - Баланс скобок - Баланс дужок.cpp

--- 2000-2999/2479 - Parentheses Balance - Баланс скобок - Баланс дужок.cpp
+++ 2000-2999/2479 - Parentheses Balance - Баланс скобок - Баланс дужок.cpp
@@ -10,32 +10,27 @@ string str;
 string f()
 {
 	if (str == "") return "Yes";
-
 	stack <bool> st;
 	for (int i = 0; i < str.length(); i++)
 	{
 		switch (str[i])
 		{
-		case '(':
-			st.push(true);
-			break;
+		case '(': st.push(true); break;
 		case ')':
-			{
-				if (st.empty()) return "No";
-				if (!st.top()) return "No";
-				st.pop();
-			}
-			break;
-		case '[':
-			st.push(false);
-			break;
+		{
+			if (st.empty()) return "No";
+			if (!st.top()) return "No";
+			st.pop();
+		}
+		break;
+		case '[': st.push(false); break;
 		case ']':
-			{
-				if (st.empty()) return "No";
-				if (st.top()) return "No";
-				st.pop();
-			}
-			break;
+		{
+			if (st.empty()) return "No";
+			if (st.top()) return "No";
+			st.pop();
+		}
+		break;
 		}
 	}
 	if (!st.empty()) return "No";
