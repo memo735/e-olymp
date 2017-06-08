26a976713b9b070b855f89509e4a22bb124fb57b
memo735 <memo735@users.noreply.github.com>
2017-06-08 14:14:08 +0400

    Update 7814 - Two-digit number from three-digit - Двузначное число из трехзначного - Двоцифрове число з трицифрового.cpp

--- 7000-7999/7814 - Two-digit number from three-digit - Двузначное число из трехзначного - Двоцифрове число з трицифрового.cpp
+++ 7000-7999/7814 - Two-digit number from three-digit - Двузначное число из трехзначного - Двоцифрове число з трицифрового.cpp
@@ -1,22 +1,15 @@
-
-
-
 #include <stdio.h>
-#include <algorithm>
 
-int n;
-int a,b,c;
-int m;
-int main(){
+int n, a, b, c, m;
 
-scanf("%d",&n);
-a=n-(n/100)*100;
-c=(n-n%10)/10;
-b=(n/100)*10 +n%10;
-m=c;
-if((b<=c)&&(b/10>=1)) m=b;
-if((a<b)&&(a/10>=1)) m=a;
-
-printf("%d",m);
-
+int main() {
+	scanf("%d", &n);
+	a = n - n / 100 * 100;
+	b = n / 100 * 10 + n % 10;
+	c = (n - n % 10) / 10;
+	m = c;
+	if (b <= c && b / 10 >= 1) m = b;
+	if (a < b && a / 10 >= 1) m = a;
+	printf("%d", m);
+	return 0;
 }
