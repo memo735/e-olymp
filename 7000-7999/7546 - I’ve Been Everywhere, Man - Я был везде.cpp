ade9e6c9bc07bed763a12a99793dda6634f688b5
memo735 <memo735@users.noreply.github.com>
2017-07-12 09:51:09 +0400

    Create 7546 - I’ve Been Everywhere, Man - Я был везде.cpp

---
+++ 7000-7999/7546 - I’ve Been Everywhere, Man - Я был везде.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	int t; cin >> t;
+	while (t-- > 0)
+	{
+		int n; cin >> n;
+		char x[30];
+		set<string> a;
+		a.clear();
+		while (n-- > 0)
+		{
+			cin >> x;
+			a.insert(x);
+		}
+		cout << a.size() << endl;
+	}
+	return 0;
+}
